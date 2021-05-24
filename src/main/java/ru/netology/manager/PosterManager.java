dc24479a5cd8494e3d9bbdf2fefd3a58bbca7e1e
Dmitrii Nasedkin <2_tone@mail.ru>
2021-05-25 00:36:57 +0300

    added Poster's classes and tests

---
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -0,0 +1,58 @@
+package ru.netology.manager;
+
+import lombok.Data;
+import ru.netology.domain.MovieItem;
+
+@Data
+public class PosterManager {
+    private MovieItem[] movies = new MovieItem[0];
+    private int anyAmountOfFilm = 10;
+
+
+    public void add(MovieItem movie) {
+        // создаём новый массив размером на единицу больше
+        int length = movies.length + 1;
+        MovieItem[] tmp = new MovieItem[length];
+        // itar + tab
+        // копируем поэлементно
+        // for (int i = 0; i < items.length; i++) {
+        //   tmp[i] = items[i];
+        // }
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public MovieItem[] getAll() {
+        int resultLenth;
+        if (movies.length > anyAmountOfFilm) {
+            resultLenth = anyAmountOfFilm;
+        } else {
+            resultLenth = movies.length;
+        }
+
+        MovieItem[] result = new MovieItem[resultLenth];
+        // перебираем массив в прямом порядке
+        // но кладём в результаты в обратном
+        for (int i = 0; i < resultLenth; i++) {
+            int index = resultLenth - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+
+    public PosterManager() {
+    }
+
+    public PosterManager(int anyAmountOfFilm) {
+        if (anyAmountOfFilm <= 0) {
+            return;
+        }
+        this.anyAmountOfFilm = anyAmountOfFilm;
+    }
+
+}
+
---
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -0,0 +1,72 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.MovieItem;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class PosterManagerTest {
+    PosterManager manager = new PosterManager(5);
+
+    @Test
+    public void shouldUseFourFilms() {
+        MovieItem first = new MovieItem(1, 1, "first", "action");
+        MovieItem second = new MovieItem(2, 2, "second", "drama");
+        MovieItem third = new MovieItem(3, 3, "third", "sport");
+        MovieItem fourth = new MovieItem(4, 4, "fourth", "comedy");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+
+        MovieItem[] expected = new MovieItem[] {fourth, third, second, first};
+        MovieItem[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUseFiveFilms() {
+        MovieItem first = new MovieItem(1, 1, "first", "action");
+        MovieItem second = new MovieItem(2, 2, "second", "drama");
+        MovieItem third = new MovieItem(3, 3, "third", "sport");
+        MovieItem fourth = new MovieItem(4, 4, "fourth", "comedy");
+        MovieItem fifth = new MovieItem(5, 5, "fifth", "action");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        MovieItem[] expected = new MovieItem[] {fifth, fourth, third, second, first};
+        MovieItem[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUseSixFilms() {
+
+        MovieItem first = new MovieItem(1, 1, "first", "action");
+        MovieItem second = new MovieItem(2, 2, "second", "drama");
+        MovieItem third = new MovieItem(3, 3, "third", "sport");
+        MovieItem fourth = new MovieItem(4, 4, "fourth", "comedy");
+        MovieItem fifth = new MovieItem(5, 5, "fifth", "action");
+        MovieItem sixth = new MovieItem(6, 6, "sixth", "thriller");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+
+        MovieItem[] expected = new MovieItem[] {fifth, fourth, third, second, first};
+        MovieItem[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
